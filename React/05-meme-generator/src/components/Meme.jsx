b60f239987b51f2dc039d0d5810cae41bb7c9d9c
kaustav-gurey <kaustav.gurey0607@gmail.com>
2023-03-02 17:13:09 +0530

    Finished the 5th project in 'React' folder

--- React/05-meme-generator/src/components/Meme.jsx
+++ React/05-meme-generator/src/components/Meme.jsx
@@ -10,10 +10,21 @@ export default function Meme(){
 
     const [allMemes, setAllMemes] = React.useState([])
 
+    // React.useEffect(()=>{
+    //     fetch("https://api.imgflip.com/get_memes")
+    //     .then(res => res.json())
+    //     .then(data => setAllMemes(data.data.memes))
+    // },[])
+
+    //pwerforming the same tasks as the above function but with async functions
+
     React.useEffect(()=>{
-        fetch("https://api.imgflip.com/get_memes")
-        .then(res => res.json())
-        .then(data => setAllMemes(data.data.memes))
+        async function getMemes() {
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setAllMemes(data.data.memes)
+        }
+        getMemes()
     },[])
 
     function getNewImg(){
